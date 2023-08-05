ddef1cc36a01ae97439761faa460c6fd59139a10
Tyler Burns <burns.tyler@gmail.com>
2023-08-05 13:08:58 +0200

    added website graph build to shell script

--- build.sh
+++ build.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
+
+# Buld the site
 emacs -Q --script build_site.el
+
+# Run the site graph builder
+cd /Users/tylerburns/Documents/projects/website_graph_view/
+source .wg_venv/bin/activate
+cd src
+python make_website_graph.py
+deactivate
+echo "website graph build complete!"
