d8a3eaa2461968e34449f0330cc236bb150867b7
Tyler Burns <burns.tyler@gmail.com>
2026-05-23 18:05:20 +0200

    now date renders on the top of the articles

--- build_site.el
+++ build_site.el
@@ -27,6 +27,7 @@
 
 ;; Load the publishing system
 (require 'ox-publish)
+(require 'subr-x)
 
 (defvar site-html-head
   (concat
@@ -38,6 +39,43 @@
   "<div style=\"position: fixed; top: 10px; right: 10px;\">
          <button class=\"toggle-theme-btn\" onclick=\"toggleDarkMode()\">Light/Dark</button>
        </div>")
+
+(defun site-html-escape (text)
+  "Escape TEXT for use in generated HTML."
+  (replace-regexp-in-string
+   "\"" "&quot;"
+   (replace-regexp-in-string
+    ">" "&gt;"
+    (replace-regexp-in-string
+     "<" "&lt;"
+     (replace-regexp-in-string "&" "&amp;" text t t)
+     t t)
+    t t)
+   t t))
+
+(defun site-keyword-value (keyword)
+  "Return the first Org KEYWORD value in the current buffer."
+  (let ((case-fold-search t))
+    (save-excursion
+      (goto-char (point-min))
+      (when (re-search-forward
+             (format "^[ \t]*#\\+%s:[ \t]*\\(.*\\)$" (regexp-quote keyword))
+             nil t)
+        (string-trim (match-string-no-properties 1))))))
+
+(defun site-insert-article-date (backend)
+  "Insert article date metadata near the top of HTML exports."
+  (when (eq backend 'html)
+    (let ((date (site-keyword-value "DATE")))
+      (when (and date (not (string-empty-p date)))
+        (goto-char (point-min))
+        (while (looking-at "^[ \t]*#\\+.*\n")
+          (forward-line 1))
+        (insert
+         (format "#+HTML: <div class=\"article-meta\"><span class=\"article-date\">%s</span></div>\n\n"
+                 (site-html-escape date)))))))
+
+(add-hook 'org-export-before-parsing-hook #'site-insert-article-date)
 
 (setq org-html-validation-link nil
       org-html-head-include-scripts nil
@@ -58,7 +96,7 @@
              :recursive t
              :base-directory "./content"
              :publishing-directory "./public"
-             :with_date t
+             :with-date nil
              :with-author nil           ;; Don't include author name
              :with-creator t            ;; Include Emacs and Org versions in footer
              :with-toc nil                ;; Include a table of contents
